fc12df9797842b186d425d42fbbd8aa65e7f1c4a
liuzhijian <464847593@qq.com>
2018-05-28 23:19:17 +0800

    更新版本

--- build/dev-server.js
+++ build/dev-server.js
@@ -112,7 +112,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+var uri = 'http://192.168.0.101:' + port
 
 devMiddleware.waitUntilValid(function () {
   console.log('> Listening at ' + uri + '\n')
--- src/components/navFooter.vue
+++ src/components/navFooter.vue
@@ -1,23 +1,23 @@
 <template>
   <tabbar>
     <tabbar-item selected link="/" @click.native="index=0">
-      <img slot="icon" src="../assets/img/movie.png" v-if="index!=0">
-      <img slot="icon" src="../assets/img/movie_blue.png" v-if="index==0">
+      <img slot="icon" src="../assets/img/movie.png" v-if="index!==0">
+      <img slot="icon" src="../assets/img/movie_blue.png" v-if="index===0">
       <span slot="label">电影</span>
     </tabbar-item>
     <tabbar-item link="/Cinema" @click.native="index=1">
-      <img slot="icon" src="../assets/img/cinema.png" v-if="index!=1">
-      <img slot="icon" src="../assets/img/cinema_blue.png" v-if="index==1">
+      <img slot="icon" src="../assets/img/cinema.png" v-if="index!==1">
+      <img slot="icon" src="../assets/img/cinema_blue.png" v-if="index===1">
       <span slot="label">影院</span>
     </tabbar-item>
     <tabbar-item link="/Find" @click.native="index=2">
-      <img slot="icon" src="../assets/img/find.png" v-if="index!=2">
-      <img slot="icon" src="../assets/img/find_blue.png"  v-if="index==2">
+      <img slot="icon" src="../assets/img/find.png" v-if="index!==2">
+      <img slot="icon" src="../assets/img/find_blue.png"  v-if="index===2">
       <span slot="label">发现</span>
     </tabbar-item>
     <tabbar-item link="/Mine" @click.native="index=3">
-      <img slot="icon" src="../assets/img/mine.png"  v-if="index!=3">
-      <img slot="icon" src="../assets/img/mine_blue.png"  v-if="index==3">
+      <img slot="icon" src="../assets/img/mine.png"  v-if="index!==3">
+      <img slot="icon" src="../assets/img/mine_blue.png"  v-if="index===3">
       <span slot="label" @click.native="index=1">我的</span>
     </tabbar-item>
   </tabbar>
