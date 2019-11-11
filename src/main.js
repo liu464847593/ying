72f29c2bc07f5404204272e18cc0abcc0394649a
liuzhijian <liuzhijian@do1.com.cn>
2019-11-11 21:33:04 +0800

    refactor: main.js 拆分出router

--- src/main.js
+++ src/main.js
@@ -2,38 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
+import router from './router/index'
 import App from './App'
-import Home from './views/movies'
-import Cinema from './views/cinema'
-import Find from './views/find'
-import Mine from './views/mine'
-import Login from './components/login'
 import { AjaxPlugin } from 'vux'
 
 Vue.use(AjaxPlugin)
-Vue.use(VueRouter)
-
-const routes = [{
-  path: '/',
-  component: Home
-}, {
-  path: '/Cinema',
-  component: Cinema
-}, {
-  path: '/Find',
-  component: Find
-}, {
-  path: '/Mine',
-  component: Mine
-}, {
-  path: '/Login',
-  component: Login
-}]
-
-const router = new VueRouter({
-  routes
-})
 
 FastClick.attach(document.body)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+import Home from '../views/movies'
+import Cinema from '../views/cinema'
+import Find from '../views/find'
+import Mine from '../views/mine'
+import Login from '../components/login'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  routes: [{
+    path: '/',
+    component: Home
+  }, {
+    path: '/Cinema',
+    component: Cinema
+  }, {
+    path: '/Find',
+    component: Find
+  }, {
+    path: '/Mine',
+    component: Mine
+  }, {
+    path: '/Login',
+    component: Login
+  }]
 })
